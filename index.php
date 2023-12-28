e63101e7e181bb552e089f1ccd62607d4f571c79
Freddybicandy50 <fredos112033@gmail.com>
2023-12-28 19:50:15 +0200

    datesite

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 if($_SERVER['REQUEST_URI']=='/') require("controllers/library.php");
 else if($_SERVER['REQUEST_URI']=='/filter') require("controllers/filter.php");
 else if($_SERVER['REQUEST_URI']=='/about') require("controllers/about.php");
-else if($_SERVER['REQUEST_URI']=='/date') require("date.php");
+else if($_SERVER['REQUEST_URI']=='/pleasedateme/date') require("datewebsite/date.html");
+else if($_SERVER['REQUEST_URI']=='/pleasedateme/SHESAIDYESS!') require("datewebsite/yes.html");
 else require("controllers/404.php");
 
 ?>
