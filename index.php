9bbd745634c3291ff999f91bcefc4dc7ad9b47c4
freddybicandy50 <fredos112033@gmail.com>
2023-12-28 17:34:24 +0200

    date

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 if($_SERVER['REQUEST_URI']=='/') require("controllers/library.php");
 else if($_SERVER['REQUEST_URI']=='/filter') require("controllers/filter.php");
 else if($_SERVER['REQUEST_URI']=='/about') require("controllers/about.php");
+else if($_SERVER['REQUEST_URI']=='/date') require("date.php");
 else require("controllers/404.php");
 
 ?>
