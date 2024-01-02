33e13f20e5ce1d8edc44a599c0835c8ebb8fb37a
FreddyBicandy50 <fredos112033@gmail.com>
2024-01-02 20:26:55 +0200

    theme fix

--- main/partials/dropdown.php
+++ main/partials/dropdown.php
@@ -36,10 +36,10 @@
          Public_hosting
          </p></a>");
       print("
-         <p id='Title' class='font-bold tracking-tight sm:text-5xl' style='color:#1d2033; margin-top: -45px; margin-left:220px;width:3%'>→</p>
+         <p id='arrow' class='font-bold tracking-tight sm:text-5xl' style='color:#1d2033; margin-top: -45px; margin-left:220px;width:3%'>→</p>
          <a href='/console'>
          <img style='margin-top: -35px; margin-left:265px;width:3%' src='https://www.svgrepo.com/show/504947/termius.svg'/>
-          <p class='font-bold tracking-tight text-gray-500 sm:text-1xl' style='color:#1d2033; margin-top: -29px; margin-left:300px;width:3%'>(<i>console</i>)</p>         
+          <p id='console' class='font-bold tracking-tight text-gray-500 sm:text-1xl' style='color:#1d2033; margin-top: -29px; margin-left:300px;width:3%'>(<i>console</i>)</p>         
          </a>
       </a>");
       print(
