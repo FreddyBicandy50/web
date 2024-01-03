afb9c974a379cae0bc28b41dfef9e3177019869a
Freddybicandy50 <fredos112033@gmail.com>
2024-01-03 21:13:46 +0200

    user token

--- main/functions/fetch_user.php
+++ main/functions/fetch_user.php
@@ -2,5 +2,13 @@
 
 session_start();    
 $_SESSION['username']=session_id();
-
-if ($_SESSION['username']=='t0debrj8aat4951quilj30bfb5' || $_SESSION['username']=='33c09pl1758fjducdrkdslfbkm' ) $_SESSION['username']='Freddy-PC';
+$users=[    
+            't0debrj8aat4951quilj30bfb5',
+            'a56af4e22022b9f95113f3a399eea270',
+            '33c09pl1758fjducdrkdslfbkm',
+            ''];
+foreach ($users as $user){
+    if ($_SESSION['username']==$user) {
+        $_SESSION['username']='Freddy-PC';
+    }
+}
