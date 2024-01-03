9f655b4f6705f311b18c2617f4e9106dcb0aa657
FreddyBicandy50 <fredos112033@gmail.com>
2024-01-03 21:14:34 +0200

    test

--- main/functions/fetch_user.php
+++ main/functions/fetch_user.php
@@ -6,7 +6,7 @@ $users=[
             't0debrj8aat4951quilj30bfb5',
             'a56af4e22022b9f95113f3a399eea270',
             '33c09pl1758fjducdrkdslfbkm',
-            ''];
+            'a56af4e22022b9f95113f3a399eea270'];
 foreach ($users as $user){
     if ($_SESSION['username']==$user) {
         $_SESSION['username']='Freddy-PC';
