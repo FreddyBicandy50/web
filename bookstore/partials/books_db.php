041c4af324a576ccf8fe7af473750be1a3e1d01c
Freddybicandy50 <fredos112033@gmail.com>
2024-01-04 22:48:11 +0200

    book database

--- bookstore/partials/books_db.php
+++ bookstore/partials/books_db.php
@@ -1,5 +1,8 @@
 <?php
 
-$connect = db("test");
+if ($_SERVER['HTTP_HOST'] =="kali-wsl.tail4bb6a.ts.net" || $_SERVER['HTTP_REFERER'] =='raspberrypi.tail4bb6a.ts.net')
+        $connect=db('test');
+else  $connect=db('if0_35693876_bookstore');
+
 if (!$connect) die("error Connecting to Database error code:" . mysqli_connect_error());
 else $result = mysqli_query($connect, "SELECT * FROM Books;");
