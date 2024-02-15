4fcdbf2fcbb6adf4a048f52fba48ad2e9f22b202
Freddy Bicandy <79138306+FreddyBicandy50@users.noreply.github.com>
2024-02-15 06:31:02 +0000

    fixed projects

--- .main/pages/main.php
+++ .main/pages/main.php
@@ -69,6 +69,9 @@
         <?php
         if ($_SERVER["HTTP_HOST"] != 'freddybicandy.42web.io') {
           print("<div class='mt-10 flex items-center justify-center gap-x-6'>
+          <a href='$carsNum_main' target='_blank' class='rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+            Plate Number</a>
+      
           <a href='$dateme_main' target='_blank' class='rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
             Date me</a>
             <a href='$bookstore' target='_blank' class='rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
