031d4107a03966de9dfbfc2b023f4991d4409081
Freddy Bicandy <79138306+FreddyBicandy50@users.noreply.github.com>
2024-02-15 06:21:33 +0000

    fixed projects

--- cars_num/controller/router_carsNum.php
+++ cars_num/controller/router_carsNum.php
@@ -1,9 +1,9 @@
 <?php
     
-    if(URL($carsNum_main) || URL($carsNum_main.'/')){
-      die(require("cars_num/page/search.php"));
-  }
+  //   if(URL($carsNum_main) || URL($carsNum_main.'/')){
+  //     die(require("cars_num/page/search.php"));
+  // }
   
-    if(URL($carsNum_result) ){
-      die(require("cars_num/page/main.php"));
-  }
+  //   if(URL($carsNum_result) ){
+  //     die(require("cars_num/page/main.php"));
+  // }
