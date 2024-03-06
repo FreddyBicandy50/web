5de65255dad7c90282ce56be84c690aac2566a6d
Freddy Bicandy <79138306+FreddyBicandy50@users.noreply.github.com>
2024-03-06 19:57:50 +0000

    days Counter

---
+++ daysCounter/controller/router_dayscounter.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "<h1>Router test</h1>";
--- router.php
+++ router.php
@@ -7,3 +7,4 @@ require("webconsole/controller/webconsole.php");
 require("notes/controller/router_notes.php");
 require("schedule/controller/router_schedule.php");
 require("cars_num/controller/router_carsNum.php");
+require("daysCounter/controller/router_dayscounter.php");
