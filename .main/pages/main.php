aff6c6e421227d309ff5d15212bec79eb075c355
FreddyBicandy50 <fredos112033@gmail.com>
2024-03-21 11:23:42 +0200

    server fix

--- .main/pages/main.php
+++ .main/pages/main.php
@@ -65,7 +65,7 @@
         </p>
         <!-- Project Buttons -->
         <?php
-        if ($_SERVER["HTTP_HOST"] != 'freddybicandy.42web.io') {
+        if ($_SERVER["HTTP_HOST"] != $server_public) {
           print("<div class='mt-10 flex items-center justify-center gap-x-6'>
           <a href='$carsNum_main' target='_blank' class='rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
             Plate Number</a>
