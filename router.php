c4ea9fc198a919a758eec8b3703115555b0a5ef4
FreddyBicandy50 <fredos112033@gmail.com>
2024-04-14 16:03:14 +0300

    reality

--- router.php
+++ router.php
@@ -4,6 +4,4 @@ require(".main/test/controller/test.php");
 require("bookstore/controller/router_bookstore.php");
 //require("notes/controller/router_notes.php");
 require("cars_num/controller/router_carsNum.php");
-require("datewebsite/controller/router_dateme.php");
-require("twinflame/controller/router_twin.php");
 require("NetworkSolutionsWeb/controller/nsb_router.php");
