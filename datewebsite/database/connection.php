98dfc7d610f822cf184e88bfdcf3ffd0bf0e6bc3
FreddyBicandy50 <fredos112033@gmail.com>
2024-04-14 16:00:17 +0300

    reality

--- datewebsite/database/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Get the session username
-$id = $_SESSION['username'];
-require('.main/database/connect.php');
-if (is_public($_SERVER[$name]))
-    $db = connection('if0_35693876_dateme');
-else
-    $db = connection('test');
--- datewebsite/partials/vars.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$dateme_main = '/gowme/';
-$dateme_yes = '/SHESAIDYESS!';
