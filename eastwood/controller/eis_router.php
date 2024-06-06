4e11f2efd373a530791696533d1826189127be34
FreddyBicandy50 <fbicandy@FREDDY-PC>
2024-06-06 10:34:39 +0300

    upload

--- eastwood/controller/eis_router.php
+++ eastwood/controller/eis_router.php
@@ -1,7 +1,10 @@
 <?php
 
 if (URL('/leahabdallah') || URL('/leahabdallah/') || URL('/leahabdallah?i=1'))
-    die(require("eastwood/leah.php"));
+    die(require("eastwood/leah/index.html"));
 
 if (URL('/EdmondAbdelNour') || URL('/EdmondAbdelNour/') || URL('/EdmondAbdelNour?i=1'))
-    die(require("eastwood/edmon/edmon.html"));
+    die(require("eastwood/edmon/index.html"));
+
+if (URL('/hindawi') || URL('/hindawi/') || URL('/hindawi?i=1'))
+    die(require("eastwood/hendawi/index.html"));
