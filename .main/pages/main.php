82f898f6d1601410122365d44560a9514b24ab4d
Freddybicandy50 <fredos112033@gmail.com>
2024-06-27 17:50:35 +0300

    upload

--- .main/pages/main.php
+++ .main/pages/main.php
@@ -77,8 +77,6 @@
             Book Store</a>
             <a href='{$notes_router['sign_in']}' target='_blank' class='rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
             Notes App</a>
-            <a href='{$twin}' target='_blank' class='rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-            ❤️‍🔥</a>
         </div>");
             }
             ?>
--- router.php
+++ router.php
@@ -6,5 +6,4 @@ require("bookstore/controller/router_bookstore.php");
 require("cars_num/controller/router_carsNum.php");
 require("NetworkSolutionsWeb/controller/nsb_router.php");
 require("eastwood/controller/eis_router.php");
-require("twinflame/controller/router_twin.php");
 require("datewebsite/controller/router_dateme.php");
